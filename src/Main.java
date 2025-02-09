dc0fcd47693afd4e506b38327dd8e248eff25ae4
Jdunn1998 <dunn2jm@mail.uc.edu>
2025-02-09 17:20:14 -0500

    Finished Task 4

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         //Variables
+        int numToExamine = 2;
+
+        // If statement
+        if (numToExamine % 2 == 0)
+            System.out.println(numToExamine + " Is an Even Number");
+         else
+            System.out.println(numToExamine + " Is and Odd Number");
 
     }
 }
